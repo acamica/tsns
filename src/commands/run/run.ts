19643aae0ff71313a88b30f1225def0127c62826
Facundo Rubin <facundorubin@gmail.com>
2018-05-16 17:31:11 -0300

    refactor(build): move `buildAndPipeOutput ` function to a build command

--- src/commands/run/run.ts
+++ src/commands/run/run.ts
@@ -1,21 +1,14 @@
-// import {exec} from 'shelljs';
-import {build, IBuildOptions} from '../build/build';
+import {build} from '../build/build';
 import { readProjectPackageJSON, tryFiles } from '../../utils';
 import {Observable} from 'rxjs';
 import {exec} from '../../utils/rx-exec';
 import paths from '../../utils/paths';
+import { buildAndPipeOutput } from '../build/build';
 import {join} from 'path';
 import {pipeOutput, closeWithErrorWhenStatusCode} from '../../utils/rx-exec';
 
 // Single run recipy
 export const runNode = (main: string) => exec(`node ${main}`);
-
-export const buildAndPipeOutput = (options: IBuildOptions) =>
-                                        build(options)
-                                            .do(pipeOutput) // TODO: Shouldn't do side effect here
-                                            .switchMap(process => closeWithErrorWhenStatusCode(process.close$))
-                                            // Finish the chain once the process has finished
-                                            .first();
 
 export const runOnce = (main: string, options: IRunOptions) => {
     const buildOnce$ = buildAndPipeOutput({watch: undefined});
